f85140ef8051de8b105b543901f7c77cf005d9de
gabrieltrindademb0-wq <gabrieltrindademb0@gmail.com>
2026-06-13 17:23:47 -0300

    up

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="crossrc-v6";
+const CACHE="crossrc-v7";
 self.addEventListener("install",e=>{
   self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
